dd59e11651d22820d13e3532f74c4941069c328f
ardaatahan <63500745+ardaatahan@users.noreply.github.com>
2020-09-03 15:46:54 +0300

    add revalidation

--- pages/index.js
+++ pages/index.js
@@ -21,6 +21,7 @@ export const getStaticProps = async () => {
     props: {
       products,
     },
+    revalidate: 1,
   };
 };
 
--- pages/product/[productId].js
+++ pages/product/[productId].js
@@ -19,6 +19,7 @@ export const getStaticProps = async ({ params: { productId } }) => {
     props: {
       product,
     },
+    revalidate: 1,
   };
 };
 
